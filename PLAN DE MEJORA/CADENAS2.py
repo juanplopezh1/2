ab32c9f947f1309bb0f841ac6b22c022495d7135
juanplopezh1 <116467169+juanplopezh1@users.noreply.github.com>
2022-11-24 20:38:03 -0500

    Delete CADENAS2.py

--- PLAN DE MEJORA/CADENAS2.py
+++
@@ -1,5 +0,0 @@
-#Escribir un programa que pregunte el nombre completo del usuario en la consola y después muestre por pantalla el nombre completo del usuario tres veces
-name = input("¿Cómo te llamas? ")
-print(name.lower())
-print(name.upper())
-print(name.title())
