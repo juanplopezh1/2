7bf752e82fd264f2a2d37715b9d30565117bc758
juanplopezh1 <116467169+juanplopezh1@users.noreply.github.com>
2022-11-24 20:38:57 -0500

    Delete LISTAS O TUPLAS.py

--- PLAN DE MEJORA/LISTAS O TUPLAS.py
+++
@@ -1,4 +0,0 @@
-#Escribir un programa que almacene las asignaturas de un curso (por ejemplo
-#Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por pantalla.
-lista = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
-print(lista)
