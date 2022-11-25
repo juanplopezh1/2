af9e332873e87b66d301ddb40342f38d835c25df
juanplopezh1 <116467169+juanplopezh1@users.noreply.github.com>
2022-11-24 20:38:37 -0500

    Delete CONDICIONALES3.py

--- PLAN DE MEJORA/CONDICIONALES3.py
+++
@@ -1,7 +0,0 @@
-#Escribir un programa que almacene la cadena de caracteres contraseña en una variable
-j = "contraseña"
-password = input("Introduce la contraseña: ")
-if j == password.lower():
-    print("La contaseña coincide")
-else:
-    print("La contraseña no coincide")
