274cc716bf5bf76c21362ce4b3579a85f7715a8c
juanplopezh1 <116467169+juanplopezh1@users.noreply.github.com>
2022-11-25 02:25:48 -0500

    Add files via upload

---
+++ PLAN DE MEJORA/condi2.py
@@ -0,0 +1,12 @@
+variable = 2
+if variable> 3:
+    print("la variable es mayor que tres ")
+    print("me parece poco")
+elif variable <3:
+    print("la variable es menor que tres ")
+    print("me parece poco")
+else:
+    print("la variable es tres ")
+    print("justo tres, ni mas ni menos ")
+print("esto siempre se muestra")
+    
