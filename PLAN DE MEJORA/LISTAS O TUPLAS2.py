644f32fcf349dcd71111504640fcd1e87867ea07
juanplopezh1 <116467169+juanplopezh1@users.noreply.github.com>
2022-11-24 20:38:51 -0500

    Delete LISTAS O TUPLAS2.py

--- PLAN DE MEJORA/LISTAS O TUPLAS2.py
+++
@@ -1,4 +0,0 @@
-#Escribir un programa que almacene las asignaturas de un curso 
-listas = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
-for listas in listas:
-    print("Yo estudio " + listas)
