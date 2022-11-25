127569497397e971f0a17e8611c4ab2dddd14f14
juanplopezh1 <116467169+juanplopezh1@users.noreply.github.com>
2022-11-24 20:38:20 -0500

    Delete CONDICIONALES.py

--- PLAN DE MEJORA/CONDICIONALES.py
+++
@@ -1,6 +0,0 @@
-#Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
-j = int(input("¿Cuál es tu edad? "))
-if j < 18: 
-    print ("Eres menor de edad.")
-else:
-    print("Eres mayor de edad.")
