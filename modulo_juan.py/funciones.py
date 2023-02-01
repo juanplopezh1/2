f857291ee67293a2d2f1126240bea9a49c724211
juanplopezh1 <116467169+juanplopezh1@users.noreply.github.com>
2023-02-01 07:33:35 -0500

    Add files via upload

---
+++ modulo_juan.py/funciones.py
@@ -0,0 +1,17 @@
+#modulo
+#funciones
+
+def sumar(a,b):
+    return a+b
+
+def restar(a,b):
+    return a-b
+
+def multiplicar(a,b):
+    return a*b
+
+def dividir(a,b):
+    return a /b
+
+def potencia(a,b):
+    return a**b
---
+++ modulo_juan.py/mainjuan.py
@@ -0,0 +1,23 @@
+import funciones 
+n1 =int(input("ingrese un numero: "))
+n2=int(input("ingrese el segundo numero: "))
+print(";;operaciones matematicas ::")
+print("""
+1- sumar
+2-restar
+3-multiplicar
+4-dividir
+5-potencia""")
+opccion=int(input("ingrese la operacion deseada: "))
+if opccion==1:
+    print(funciones.sumar(n1,n2))
+elif opccion ==2:
+    print(funciones.restar(n1,n2))
+elif opccion==3:
+    print(funciones.multiplicar(n1,n2))
+elif opccion==4:
+    print(funciones.dividir(n1,n2))
+elif opccion==5:
+    print(funciones.potencia(n1,n2))
+
+else: print("el numero no es valido ")
